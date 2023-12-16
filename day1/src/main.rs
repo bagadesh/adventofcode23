26ae5cbeac3ad1fda61e90f983b0c1b0cf7642a5
bagadeshfox <bagadesh.r@fox.com>
2023-12-16 19:39:27 +0530

    day1 optimize

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,26 +4,30 @@ use std::io::Error;
 fn main() -> Result<(), Error>{
     let collabration_doc = fs::read_to_string("input.txt")?;
 
-    let sum: u32 = collabration_doc.split("\n").map(
-        |item|{
-            let mut first_digit = 0;
-            let mut second_digit = 0;
-            for l in item.chars() {
-                if l.is_digit(10) {
-                    first_digit = l.to_digit(10).unwrap();
-                    break;
-                } 
-            }  
-            for l in item.chars().rev() {
-                if l.is_digit(10) {
-                    second_digit = l.to_digit(10).unwrap();
-                    break;
-                } 
-            }  
-            (first_digit * 10) + second_digit
-        }
-    ).sum();
+    let sum: u32 = extract_digits(&collabration_doc)
+        .into_iter()
+        .fold(0, |acc,(f,s) | acc + (f*10)+ s);
     
     println!("sum {}", sum);
     Ok(())
+}
+
+fn extract_digits(input: &str) -> Vec<(u32,u32)> {
+    return  input.split("\n").map(|item| {
+        let mut first_digit = 0;
+        let mut second_digit = 0;
+        for l in item.chars() {
+            if l.is_digit(10) {
+                first_digit = l.to_digit(10).unwrap();
+                break;
+            } 
+        }  
+        for l in item.chars().rev() {
+            if l.is_digit(10) {
+                second_digit = l.to_digit(10).unwrap();
+                break;
+            } 
+        }  
+        (first_digit, second_digit)
+    }).collect();
 }
